7311a3aaff4c6be1a964071d9c323cf375a11ea8
nicholas-amarante <nicholasfelipe361@gmail.com>
2026-04-13 23:13:48 -0300

    adição de novos influencers

--- akinator.pl
+++ akinator.pl
@@ -1,3 +1,5 @@
+%Como rodar o programa: 1-Instala swipl 2-Abre o swipl no terminal e executa o progrma com [akinator]. 
+
 jogar:-
     retractall(afirmacao(_)),
     adivinha(Influencer),
@@ -7,14 +9,32 @@ jogar:-
     write('Desculpa, não consegui adivinhar. É alguém muito nichado'), nl.
 
 adivinha(filipe_deschamps):-
-    eh(noticias_diarias),
-    eh(fala_sobre_ia).
+    eh(fala sobre noticias diarias),
+    eh(fala sobre ia).
 
 adivinha(fabio_akita):-
-    eh(videos_longos),
-    eh(fala_sobre_ancestralidade_tech),
-    eh(fala_sobre_ia),
-    eh(videos_tecnicos).
+    eh(produz videos longos),
+    eh(fala sobre ancestralidade tech),
+    eh(fala sobre ia),
+    eh(produz videos tecnicos).
+
+adivinha(codigo fonte tv):-
+    eh(fala sobre noticias diarias),
+    eh(fala sobre curiosidades),
+    eh(fala sobre ia).
+
+adivinha(gustavo guanabara):-
+    eh(ensina muito bem),
+    eh(produz videos tecnicos),
+    eh(tem relacao com gafanhotos).
+
+adivinha(karol attekita):-
+    eh(ensina muito bem),
+    eh(fala sobre curiosidades),
+    eh(fala sobre ia),
+    eh(produz videos tecnicos).
+
+
 
 eh(Caracteristica):-
     (afirmacao(Caracteristica)->true;
